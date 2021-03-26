4f89d9d6884eeb66348df713ac492086a7ab6755
kainhuck <xiaorang.hu@tuya.com>
2021-03-26 14:01:14 +0800

    add encode string

--- serialize/json/encode.go
+++ serialize/json/encode.go
@@ -8,3 +8,8 @@ import (
 func EncodeBytes(key interface{}) ([]byte, error) {
 	return json.Marshal(key)
 }
+
+func EncodeString(key interface{}) (string, error) {
+	bts, err := json.Marshal(key)
+	return string(bts), err
+}
